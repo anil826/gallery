11d679fe935571429ae93d79ab3bc6f830074bd6
anil <anil.enbake@gmail.com>
2015-09-14 11:57:45 +0530

    json data formated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
     puts "----------------------hello-----------"
     if params[:email] == 'anil.enbake@gmail.com' && params[:api_token] == "64a0aee7a7ed1e6e2821160686de63c8da21e7c65182d99d2fc578a5ae2988bf"
       logger.info "--------------------Authentication success...---------------------"
-       render :json => get_user
+       render :json => get_user.albums.first
     else
       render :json => {error: "bad request type"}
     end
